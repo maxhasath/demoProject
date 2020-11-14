0c7536028f0ecdd1ad1472123a81404881fd6e00
keshawa <maxhasath@gmail.com>
2020-11-14 18:58:26 +0530

    first commit

---
+++ src/main/java/data/LoginData.java
@@ -0,0 +1,24 @@
+package data;
+
+public class LoginData {
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    private String title;
+    private String userName;
+    private String password;
+
+
+
+
+}
